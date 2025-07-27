f30b4aef9d329894378c04b576a81ec5a4847bfa
SkyJumper409x <alexander.schaaf.jr@icloud.com>
2025-07-27 13:22:16 +0200

    woops

--- sendougear/src/main/java/xyz/skyjumper409/Main.java
+++ sendougear/src/main/java/xyz/skyjumper409/Main.java
@@ -93,7 +93,7 @@ public class Main {
     private static boolean checkGear(Gear gear, Ability[][] testAbilities) {
         for (int i = 0; i < 3; i++)
             for (int j = 0; j < 4; j++)
-                if(!gear.get(i).abilities[j].equals(ImgStuff.correctEffects[i][j]))
+                if(!gear.get(i).abilities[j].equals(testAbilities[i][j]))
                     return false;
         return true;
     }
