4eb0af46cf8f30e757f2d4cfc965825056b0d4b4
SkyJumper409x <alexander.schaaf.jr@icloud.com>
2025-07-28 01:24:04 +0200

    forgot a file

--- sendougear/src/main/java/xyz/skyjumper409/sendougear/data/Gear.java
+++ sendougear/src/main/java/xyz/skyjumper409/sendougear/data/Gear.java
@@ -26,9 +26,20 @@ public class Gear {
     public String toAbilitiesString() {
         String s = "";
         for (int i = 0; i < pieces.length; i++) {
-            s += "\n";
             s += pieces[i].toAbilitiesString();
+            s += "\n";
         }
-        return s.substring(1);
+        s += "https://sendou.ink/analyzer?weapon=0&build=";
+        for (int i = 0; i < pieces.length; i++) {
+            Ability[] abilities = pieces[i].abilities;
+            for (int j = 0; j < abilities.length; j++) {
+                s += abilities[j].shortName;
+                s += "%2C"; // just commas, but sendou.ink urlencodes em for some reason
+            }
+        }
+        if(s.contains("UNKNOWN"))
+            s = s.replaceAll("UNKNOWN", "U");
+        s = s.substring(0, s.length() - 3) + "&build2=U,U,U,U,U,U,U,U,U,U,U,U&lde=0&focused=1".replaceAll(",", "%2C");
+        return s;
     }
 }
